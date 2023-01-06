6dde1dff777b390e29da4ee662f0e3db3190d6a2
zahraraci <azahracip01@gmail.com>
2023-01-06 15:40:17 +0700

    add konfigurasi database

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
+import pymysql
+
 from app import app
 from flask import render_template
+from flaskext.mysql import MySQL
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Alifia'}
     return render_template('index.html', title='Home', user=user)
+
+# database
+mysql = MySQL(app)
+# konfigurasi database
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '123456'
+app.config['MYSQL_DATABASE'] = 'sistem_informasi_siswa'
+
