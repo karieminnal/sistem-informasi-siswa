1fd1950ed94db9112dd3e43d8b947b4e6eb50e22
zahraraci <azahracip01@gmail.com>
2023-01-07 10:28:54 +0700

    insert query

--- app/routes.py
+++ app/routes.py
@@ -1,37 +1,43 @@
-import pymysql
+import psycopg2
 
 from app import app
-from flask import Flask, render_template, request, jsonify
-from flaskext.mysql import MySQL
+from flask import Flask, render_template, request, flash, url_for, redirect
 
 # konfigurasi database
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'sistem_informasi_siswa'
-
-mysql = MySQL(app)
-
-# @app.route('/')
-# def contoh():
-#     return render_template('index.html')
-
+conn = psycopg2.connect(host="localhost", database="sistem_informasi_siswa", user="postgres", password="123456")
+cur = conn.cursor()
 # Read
 @app.route('/')
 def index():
     # koneksi database
-    conn = mysql.connect()
-    cur = conn.cursor()
     cur.execute("SELECT * FROM tabel_siswa")
     data = cur.fetchall()
-    cur.close()
-    conn.close()
+    # cur.close()
+    # conn.close()
 
     return render_template('index.html', students = data)
 
 # Create
-# cur.execute("INSERT INTO users (name, email) VALUES (%s, %s)", (name, email))
-# conn.commit()
+@app.route('/insert', methods = ['POST'])
+def insert():
+    if request.method == "POST":
+        flash("Data Inserted Sucessfully")
+        nisn = request.form['nisn']
+        niss = request.form['niss']
+        nama = request.form['nama']
+        tanggal_lahir = request.form['tanggal_lahir']
+        tempat_lahir = request.form['tempat_lahir']
+        alamat = request.form['alamat']
+        jenis_kelamin = request.form['jenis_kelamin']
+        nama_orangtua = request.form['nama_orangtua']
+        asal_sekolah = request.form['asal_sekolah']
+        tahun_ijazah = request.form['tahun_ijazah']
+        cur.execute("INSERT INTO tabel_siswa (nisn, niss, nama, tanggal_lahir, tempat_lahir, alamat, jenis_kelamin, nama_orangtua, asal_sekolah, tahun_ijazah) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (nisn, niss, nama, tanggal_lahir, tempat_lahir, alamat, jenis_kelamin, nama_orangtua, asal_sekolah, tahun_ijazah))
+        conn.commit()
+
+        return redirect(url_for('index'))
+
+
 
 # Update
 # cur.execute("UPDATE users SET name=%s WHERE id=%s", (new_name, id))
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-from app import app
+if __name__=="__main__":
+    from app import app
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.run(debug=True)
